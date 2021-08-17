2e3880498e80ca5370ff6c1bbb34df2a8aa678b3
Sean Abe <skabe89@gmail.com>
2021-08-16 23:06:42 -0400

    ruby 2.7.2

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Dog.create([
+    {name:"Peppers", age: 12},
+    {name:"Oscar", age: 8},
+    {name: "Tasha", age: 14}
+])
