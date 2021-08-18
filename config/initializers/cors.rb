a532ae58706495e5c8399bace28459c73647dffe
Sean <skabe89@gmail.com>
2021-08-17 22:02:07 -0400

    rollback cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -9,7 +9,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins 'localhost:3000'
 
-    resource 'https://main.d3q4x93n6npvuy.amplifyapp.com/',
+    resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
