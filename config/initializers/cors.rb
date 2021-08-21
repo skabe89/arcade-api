ec81790d72cf616cd0b7633bff6a8bff2c49fa14
Sean Abe <skabe89@gmail.com>
2021-08-20 21:25:56 -0400

    orgins

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://main.d3q4x93n6npvuy.amplifyapp.com/'
+    origins '*'
 
     resource '*',
       headers: :any,
